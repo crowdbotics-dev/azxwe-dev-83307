d275c16f36623ad3e443df1e1a7a46a67ba92f31
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 10:09:01 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,129 +1,153 @@
-import React from "react";
-import { Text, View, StyleSheet, Image, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-const OrderSuccess = () => {
+const OrderInvoice = () => {
+  const [order, setOrder] = useState({});
+  useEffect(() => {
+    setOrder({
+      name: "Product name",
+      totalPrice: 10,
+      image: require("./assets/productImage.png"),
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor aliquam augue placerat mattis amet velit nisi nisl dui imperdiet et in. .",
+      promoPrice: 0,
+      deliveryFee: 0,
+      productPrice: 10,
+      status: "Delivered",
+      barcode: require("./assets/barcodeImage.png")
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.body}>
-        <View style={styles.circle}>
-          <Image
-            source={require("./assets/checkIcon.png")}
-            style={styles.check}
-          />
+      <Text style={styles.heading}>Order Id</Text>
+      <Text style={styles.subHeading}>#2345123</Text>
+      <View style={styles.invoiceCard}>
+        <View style={styles.header}>
+          <Image source={order.image} style={styles.image} />
+          <Text style={styles.name}>{order.name}</Text>
+          <Text style={styles.price}>
+            ${order.totalPrice && order.totalPrice.toFixed(2)}
+          </Text>
         </View>
-        <Text style={styles.heading}>Order Sucessful</Text>
-        <Text style={styles.subHeading}>Your order has been processed.</Text>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.fnt16}>Order&apos;s descriptions</Text>
+          <Text style={styles.description}>{order.description}</Text>
+        </View>
+        <View style={styles.detailsContainer}>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Promo</Text>
+            <Text style={styles.fnt16}>
+              ${order.promoPrice && order.promoPrice.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Delivery Fee</Text>
+            <Text style={styles.fnt16}>
+              ${order.deliveryFee && order.deliveryFee.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Product Price</Text>
+            <Text style={styles.fnt16}>
+              ${order.productPrice && order.productPrice.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Total Price</Text>
+            <Text style={styles.fnt16}>
+              ${order.totalPrice && order.totalPrice.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.flexRow}>
+          <Text style={styles.fnt16}>Status</Text>
+          <Text>{order.status}</Text>
+        </View>
+        <Image source={order.barcode} style={styles.barcode} />
       </View>
-
-      <Button buttonText="Go back" style={styles.button} />
-      <Button
-        buttonText="Track order"
-        style={styles.button}
-        color="#fff"
-        textColor="#000"
-        outlineColor="#000"
-      />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center"
-  },
-  circle: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#12D790",
-    alignSelf: "center"
-  },
-  check: {
-    width: 50,
-    height: 50
+    backgroundColor: "#fff"
   },
   heading: {
-    marginTop: 10,
-    fontSize: 32,
-    alignSelf: "center",
-    color: "#000"
+    fontSize: 24,
+    textTransform: "uppercase",
+    color: "#7C7C7C",
+    textAlign: "center"
   },
   subHeading: {
-    fontSize: 16,
+    fontSize: 24,
+    textTransform: "uppercase",
+    color: "#000",
+    textAlign: "center"
+  },
+  invoiceCard: {
+    width: 340,
+    height: 570,
+    backgroundColor: "#F0F2F7",
     alignSelf: "center",
-    marginTop: 10,
-    color: "#8A8A8E"
-  },
-  body: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  button: {
-    marginBottom: 20,
-    marginHorizontal: 20
-  }
-});
-
-export default OrderSuccess;
-
-const Button = params => {
-  const backgroundColor = params.color || "#000";
-  const textColor = params.textColor || "#fff";
-  const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.outlineColor || backgroundColor,
-    borderWidth: 1
-  };
-  const btnText = {
-    color: textColor
-  };
-  return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
-          </Text>
-          <View style={styles.childrenContainer}>{params.children}</View>
-        </Pressable>
-      </View>
-    </View>
-  );
-};
-
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    justifyContent: "center"
-  },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    elevation: 10,
-    backgroundColor: "#fff",
-    borderRadius: 10
-  },
-  btn: {
-    height: 50,
-    padding: 10,
-    paddingHorizontal: 25,
+    marginTop: 20,
     borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-
-    flexDirection: "row"
+    paddingHorizontal: 20,
+    paddingVertical: 20
   },
-  btnText: {
-    color: "#fff",
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    paddingVertical: 20,
+    borderBottomColor: "#000"
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  name: {
     fontSize: 16,
+    color: "#3E4462",
+    flex: 1,
+    marginLeft: 10
+  },
+  price: {
+    fontSize: 24,
     fontWeight: "bold"
   },
-  childrenContainer: {
-    justifyContent: "center",
+  descriptionContainer: {
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+    paddingVertical: 10
+  },
+  description: {
+    marginVertical: 5,
+    textAlign: "justify"
+  },
+  fnt16: {
+    fontSize: 16
+  },
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center"
+  },
+  detailsContainer: {
+    paddingVertical: 20,
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+    marginBottom: 10
+  },
+  barcode: {
+    width: 250,
+    height: 100,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 40
   }
 });
+
+export default OrderInvoice;
